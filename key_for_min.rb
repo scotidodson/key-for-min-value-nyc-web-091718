56a9b6430121e333fab6af23a7008cb5e7eb153e
Scoti Dodson <scotidodson@gmail.com>
2018-09-06 19:40:51 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,7 @@ def key_for_min_value(name_hash)
   price_val =""
 
   name_hash.each do |item, price|
-    if(item_val == "", price_val == "")
+    if(item_val == "")
       item_val =  item
       price_val = price 
     elsif(price < price_val)
